1687c2b74da35b58dbd7441791dc300547d433c9
Gabriel Camargo <gcamargo.dev@gmail.com>
2022-10-29 21:37:39 -0300

    changed project'j name

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,13 +5,14 @@ export default function Header() {
 	return (
 		<Container>
 			<nav>
-				<h1>work
+				<h1>
+					post
 					<span>
 					Blog
 					</span>
 				</h1>
 
-				<Input type="text" placeholder="Search a job" />
+				<Input type="text" placeholder="Search a post" />
 			</nav>
 		</Container>
 	);
